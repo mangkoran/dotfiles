87311f38bd6838943481775490ab72b18409983f
mangkoran <57625126+mangkoran@users.noreply.github.com>
2020-10-29 20:57:37 +0800

    Use new i3/config file, add sxhkd to startup

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,11 +1,14 @@
 xrandr --setprovideroutputsource modesetting NVIDIA-0
 xrandr --auto
 
+pgrep -x sxhkd > /dev/null || sxhkd &
+
 ~/.screenlayout/screenlayout.sh
 ~/.config/polybar/launch.sh
-nitrogen --restore &
+nitrogen --restore
+
 picom -b & 
 redshift &
 
 discord &
-telegram-desktop -- %u &
+telegram-desktop &
