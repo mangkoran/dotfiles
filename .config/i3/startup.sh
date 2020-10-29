53270639ac33556282cef3431eda488a3967b2aa
mangkoran <57625126+mangkoran@users.noreply.github.com>
2020-10-29 18:17:01 +0800

    Initial commit

---
+++ .config/i3/startup.sh
@@ -0,0 +1,11 @@
+xrandr --setprovideroutputsource modesetting NVIDIA-0
+xrandr --auto
+
+~/.screenlayout/screenlayout.sh
+~/.config/polybar/launch.sh
+nitrogen --restore &
+picom -b & 
+redshift &
+
+discord &
+telegram-desktop -- %u &
