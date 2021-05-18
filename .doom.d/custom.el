8c5de436fb705eeece30defed38bf9a3f342e228
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-05-18 10:18:17 +0800

    Add .doom.d/

---
+++ .doom.d/custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+    '(ansi-color-names-vector
+         ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
+    '(custom-safe-themes
+         '("990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "75b8719c741c6d7afa290e0bb394d809f0cc62045b93e1d66cd646907f8e6d43" "6daa09c8c2c68de3ff1b83694115231faa7e650fdbb668bc76275f0f2ce2a437" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "461e9e0d69636be8b5347a030f14b16c996c60a89e48c33f48bde51c32da3248" "d83e34e28680f2ed99fe50fea79f441ca3fddd90167a72b796455e791c90dc49" "4feee83c4fbbe8b827650d0f9af4ba7da903a5d117d849a3ccee88262805f40d" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "f4876796ef5ee9c82b125a096a590c9891cec31320569fc6ff602ff99ed73dca" "8f5a7a9a3c510ef9cbb88e600c0b4c53cdcdb502cfe3eb50040b7e13c6f4e78e" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" "fce3524887a0994f8b9b047aef9cc4cc017c5a93a5fb1f84d300391fba313743" default))
+ '(fci-rule-color "#7c6f64")
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d1011" "#fabd2f"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d1011" "#b8bb26"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d1011" "#928374"))
+ '(objed-cursor-color "#fb4934")
+ '(package-selected-packages '(skewer-mode evil-commentary))
+ '(pdf-view-midnight-colors (cons "#ebdbb2" "#282828"))
+    '(rustic-ansi-faces
+         ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
+ '(vc-annotate-background "#282828")
+    '(vc-annotate-color-map
+         (list
+             (cons 20 "#b8bb26")
+             (cons 40 "#cebb29")
+             (cons 60 "#e3bc2c")
+             (cons 80 "#fabd2f")
+             (cons 100 "#fba827")
+             (cons 120 "#fc9420")
+             (cons 140 "#fe8019")
+             (cons 160 "#ed611a")
+             (cons 180 "#dc421b")
+             (cons 200 "#cc241d")
+             (cons 220 "#db3024")
+             (cons 240 "#eb3c2c")
+             (cons 260 "#fb4934")
+             (cons 280 "#e05744")
+             (cons 300 "#c66554")
+             (cons 320 "#ac7464")
+             (cons 340 "#7c6f64")
+             (cons 360 "#7c6f64")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
